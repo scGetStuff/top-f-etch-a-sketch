fdae052c57e905314ddf9008406956294973027f
Scott <67354101+scGetStuff@users.noreply.github.com>
2022-02-18 22:14:39 -0500

    add ui for input, color changer, size, reset

--- script.js
+++ script.js
@@ -1,38 +1,55 @@
 "use strict"
 
-const container = document.querySelector(".container");
+const GRID = document.querySelector(".container");
 const sizeInput = document.querySelector("#size");
-const resetBtn = document.querySelector(".reset button");
-resetBtn.addEventListener('click', resetGrid);
+const options = document.getElementsByName("option");
+const colorPicker = document.querySelector("#color");
+const resetBtn = document.querySelector(".reset button")
+
+const GRID_SIZE = 600;
+
+// user input
 let gridSize = 1;
-// rather than truncate a fractional square size, let the grid adjust for rounding
-const CONTAINER_SIZE = 600;
-// TODO: this will be user input
+let optionValue = 0;
+let colorValue = 'black';
+
+// calculated
 let squareSize = 0;
 
 
-// TODO: mouseenter event and detect mouse button down
-// will replace tempory css :hover thing i started with
+options.forEach(option => {
+    option.addEventListener('change', function () {
+        optionValue = this.value;
+    })
+});
 
+colorPicker.addEventListener('change', function () {
+    colorValue = this.value;
+});
 
-function resetGrid() {
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
+resetBtn.addEventListener('click', function () {
+    emptyGrid();
     createGrid();
+});
+
+
+function emptyGrid() {
+    while (GRID.firstChild) {
+        GRID.removeChild(GRID.firstChild);
+    }
 }
 
 function createGrid() {
-
     gridSize = sizeInput.value;
     updateSquareSize();
 
     for (let row = 0; row < gridSize; row++)
-        createRow(container, row);
+        createRow(GRID, row);
 }
 
+// modify the css in memory
 function updateSquareSize() {
-    squareSize = CONTAINER_SIZE / gridSize;
+    squareSize = GRID_SIZE / gridSize;
     const rule = getSquareRule();
     console.assert(rule)
     rule.style.setProperty('width', `${squareSize}px`);
@@ -40,6 +57,8 @@ function updateSquareSize() {
 }
 
 // TODO: kind of hate this code, make it less dumb
+// cssRules in chrome broke when launching as file
+// had to set up live server to test, CORS stuff
 function getSquareRule() {
     // assumption that there is only one sheet
     console.assert(document.styleSheets.length === 1);
@@ -72,10 +91,27 @@ function createCell(parentDiv, row, col) {
     cellDiv.classList.add('square');
     cellDiv.dataset.row = row;
     cellDiv.dataset.col = col;
-    // this is the shitty inline way
-    // cellDiv.style.width = `${squareSize}px`;
-    // cellDiv.style.height = `${squareSize}px`;
+    // TODO: do something with bubbling, event deligation
+    // this is brute force, too many bindings
+    cellDiv.addEventListener('mouseenter', changeCellColor);
     parentDiv.appendChild(cellDiv);
+}
+
+// only change if an option is selected and they hold down left mouse button
+// TODO: why am i using numbers?  hard coded crap many as well be strings
+// TODO: something in the timing is screwed up, move just as you click and the
+// cursor changes to a NO symbol, circle with slash
+// breaks event code, figure it out
+function changeCellColor(e) {
+    if (optionValue == 0)
+        return;
+
+    if (e.buttons == 1) {
+        if (optionValue == 1)
+            this.style.setProperty('background-color', `${colorValue}`);
+        else if (optionValue == 2)
+            this.style = '';
+    }
 }
 
 console.clear();
